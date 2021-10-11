951821f3f323e077d6e3f8d846e362020cfa9d29
apelissier <alexispelissier2002@gmail.com>
2021-10-11 11:55:42 +0200

    ajout flask

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+import os
+
+from flask import Flask
+
+app = Flask(__name__)
+host = os.environ["HOST"]
+
+
+@app.route("/")
+def print_hi():
+    return '<h1>hi</h1>'
 
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    app.run(
+        host=host,
+        port=8001,
+        debug=True
+    )
